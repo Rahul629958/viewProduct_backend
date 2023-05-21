fcce1d32493b3a59906d3a1f944cbe3ec981fad0
Rahul629958 <75100864+Rahul629958@users.noreply.github.com>
2023-05-21 20:49:07 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -237,29 +237,20 @@ let linkObj = {};
 
 
 
-let correctionCheck=true;
 
 async function performScraping(link) {
- let axiosResponse;
+
   try{
- axiosResponse = await axios.request({
+ const axiosResponse = await axios.request({
     method: "GET",
     url: link,
     headers: {
       "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
     },
-  }); correctionCheck=true;}
-  catch(error)
-  {
-    correctionCheck=false;
-    console.log("Error generated");
-  }
+  }); 
 
 
-  
-
-  if(correctionCheck){
 
 
   var $ = cheerio.load(axiosResponse.data);
@@ -293,19 +284,6 @@ async function performScraping(link) {
   var paymentReq = $('div[data-test="pricing-type"]').text();
 
 
-}else
-{
-  var titleName="Error ! Something wrong with product link, please recheck :(";
- var ImgURL="";
- var highlightName="";
-  var descriptionName="Make sure you've entered correct link of product. It should be in format: 'https://www.producthunt.com/posts/product_name'";
-  var link="";
-  var tagList=[". . ."];
- var paymentReq="Free";
-
-  
-}
-
   scrapedData = {
     link: link,
     ImgURL: ImgURL,
@@ -315,6 +293,90 @@ async function performScraping(link) {
     Taglist: tagList,
     Plan:paymentReq
   };
+
+
+
+
+
+}
+  catch(error)
+  {
+    correctionCheck=false;
+    var titleName="Error ! Something wrong with product link, please recheck :(";
+ var ImgURL="";
+ var highlightName="";
+  var descriptionName="Make sure you've entered correct link of product. It should be in format: 'https://www.producthunt.com/posts/product_name'";
+  var link="";
+  var tagList=[". . ."];
+ var paymentReq="Free";
+    console.log("Error generated");
+
+
+    scrapedData = {
+      link: link,
+      ImgURL: ImgURL,
+      Title: titleName,
+      Highlights: highlightName,
+      Description: descriptionName,
+      Taglist: tagList,
+      Plan:paymentReq
+    };
+
+
+
+  }
+
+
+  
+
+  // if(correctionCheck){
+
+
+  // var $ = cheerio.load(axiosResponse.data);
+
+
+
+
+
+
+
+  // var imgURL = $(".styles_mediaThumbnail__LDCQN").attr("src");
+  // var posterURL = $(".styles_mediaThumbnail__LDCQN").attr("poster");
+
+  // var ImgURL = imgURL ? imgURL : posterURL;
+  // var titleName = $("h1").text();
+  // var highlightName = $("h2").text();
+  // var descriptionName = $(
+  //   ".styles_htmlText__d6xln, .color-darker-grey fontSize-16 fontWeight-400"
+  // ).text();
+
+  // var tagList = [];
+  // $(".styles_reset__opz7w").each((index, element) => {
+  //   var tagVal = $(element).find("span").text();
+  //   if (tagVal) {
+  //     tagList.push(tagVal);
+  //   }
+  // });
+
+
+
+  // var paymentReq = $('div[data-test="pricing-type"]').text();
+
+
+// }else
+// {
+//   var titleName="Error ! Something wrong with product link, please recheck :(";
+//  var ImgURL="";
+//  var highlightName="";
+//   var descriptionName="Make sure you've entered correct link of product. It should be in format: 'https://www.producthunt.com/posts/product_name'";
+//   var link="";
+//   var tagList=[". . ."];
+//  var paymentReq="Free";
+
+  
+// }
+
+ 
 
   return scrapedData;
 }
